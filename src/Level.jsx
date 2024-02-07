836d511cc281199a6f2ce2c4d878a6cbc36d8986
faheem4531 <faheem4531@gmail.com>
2024-02-08 00:22:44 +0500

    set 1st floor block & color , function

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,14 +1,29 @@
-function BlockStart() {
-  return <>
-    <mesh receiveShadow position={[0, -0.1, 0]}>
-      <boxGeometry args={[4, 0.2, 4]} />
-      <meshStandardMaterial color="limegreen" />
-    </mesh>
-  </>
+import * as THREE from 'three'
+
+THREE.ColorManagement.legacyMode = false
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
+const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
+const floor3Material = new THREE.MeshStandardMaterial({ color: 'orangered' })
+const floor4Material = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+
+function BlockStart({ position = [0, 0, 0] }) {
+
+  return <group position={position}>
+    <mesh
+      geometry={boxGeometry}
+      receiveShadow
+      position={[0, -0.1, 0]}
+      scale={[4, 0.2, 4]}
+      material={floor1Material}
+    />
+  </group>
 }
 
 export default function Level() {
   return <>
-    <BlockStart />
+    <BlockStart position={[0, 0, 0]} />
   </>
 }
