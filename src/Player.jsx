b4078a636f8b824d60763792213aa8d0124e563e
faheem4531 <faheem4531@gmail.com>
2024-02-09 04:32:03 +0500

    set forward movement for the ball

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,8 +1,33 @@
+import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
+import { useRef } from "react";
+import { useKeyboardControls } from '@react-three/drei'
 
 export default function Player() {
 
-  return <RigidBody colliders="ball" restitution={0.2} friction={1} position={[0, 1, 0]}>
+  const body = useRef()
+  const [subscribeKeys, getKeys] = useKeyboardControls()
+
+  useFrame((state, delta) => {
+
+    const { forward, backward, leftward, rightward } = getKeys()
+
+    const impulse = { x: 0, y: 0, z: 0 }
+    const torque = { x: 0, y: 0, z: 0 }
+
+    const impulseStrength = 0.6 * delta
+    const torqueStrength = 0.2 * delta
+
+    if (forward) {
+      impulse.z -= impulseStrength
+      torque.x -= torqueStrength
+    }
+
+    body.current.applyImpulse(impulse)
+    body.current.applyTorqueImpulse(torque)
+  })
+
+  return <RigidBody ref={body} colliders="ball" restitution={0.2} friction={1} position={[0, 1, 0]}>
     <mesh castShadow>
       <icosahedronGeometry args={[0.3, 1]} />
       <meshStandardMaterial flatShading color="mediumpurple" />
