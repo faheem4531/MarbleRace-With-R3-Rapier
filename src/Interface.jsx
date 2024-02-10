78cfa27d71c8e1f7d6ff10135349b60f7e8a955a
faheem4531 <faheem4531@gmail.com>
2024-02-10 17:49:04 +0500

    create the ui for the btns etc

---
+++ src/Interface.jsx
@@ -0,0 +1,34 @@
+
+const Interface = () => {
+
+  return (
+    <div className="interface">
+      {/* Time  */}
+      <div className="time">
+        0.00
+      </div>
+
+      {/* Restart  */}
+      <div className="restart">
+        Restart
+      </div>
+
+      {/* Controls  */}
+      <div className="controls">
+        <div className="raw">
+          <div className="key"></div>
+        </div>
+        <div className="raw">
+          <div className="key"></div>
+          <div className="key"></div>
+          <div className="key"></div>
+        </div>
+        <div className="raw">
+          <div className="key large"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Interface
