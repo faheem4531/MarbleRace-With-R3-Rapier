30c2ada5e47b2fed748601d74f46332c12202fad
faheem4531 <faheem4531@gmail.com>
2024-02-10 18:59:39 +0500

    basic zustand structure for global state managment

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,16 +3,21 @@ import Lights from './Lights.jsx'
 import Level from './Level.jsx'
 import { Debug, Physics } from '@react-three/rapier'
 import Player from './Player.jsx'
+import useGame from './stores/useGame.jsx'
 
 export default function Experience() {
+
+    const blockCounts = useGame((state) => state.blockCounts)
+
     return <>
+
 
         <OrbitControls makeDefault />
 
         <Physics>
             {/* <Debug /> */}
             <Lights />
-            <Level />
+            <Level count={blockCounts} />
             <Player />
         </Physics>
 
---
+++ src/stores/useGame.jsx
@@ -0,0 +1,6 @@
+import create from 'zustand'
+
+export default create(() => {
+
+  return { blockCounts: 3 }
+})
